39306fb6d79383b8b5f2fed985c3bc7e87871a5d
yossely <yossely7@gmail.com>
2018-10-27 23:51:50 -0500

    Add sticky class on element

--- projects/sticky-directive/src/lib/sticky.directive.spec.ts
+++ projects/sticky-directive/src/lib/sticky.directive.spec.ts
@@ -1,8 +1,133 @@
+import { Component, DebugElement, ViewChild, ElementRef } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { StickyDirective } from './sticky.directive';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'ngx-dummy-component',
+  styles: [`
+    #body-container {
+      background-color: yellow;
+      height: 2000px;
+      overflow: scroll;
+      padding: 10px;
+    }
+
+    .super-height {
+      background-color: black;
+      height: 5000px;
+      position: relative;
+      width: 100%;
+    }
+
+    #sticky-component {
+      background-color: green;
+      height: 50px;
+      width: 100%;
+      position: sticky;
+      top: -10px;
+      z-index: 10;
+    }
+
+    .when-sticky {
+      background-color: magenta !important;
+    }
+  `],
+  template: `
+    <div id="body-container" #scCont>
+      <div id="sticky-component" ngxSticky [scrollContainer]="scCont">
+      </div>
+      <div id="trigger-here" class="super-height">
+      </div>
+    </div>
+  `,
+})
+class DummyComponent {
+  @ViewChild('scCont') scCont: ElementRef;
+}
 
 describe('StickyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new StickyDirective();
+  let fixture: ComponentFixture<DummyComponent>;
+  let component: DummyComponent;
+  let componentDe: DebugElement;
+
+  let directiveDe: DebugElement;
+  let directive: StickyDirective;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DummyComponent,
+        StickyDirective
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DummyComponent);
+    component = fixture.componentInstance;
+    componentDe = fixture.debugElement;
+
+    directiveDe = componentDe.query(By.directive(StickyDirective));
+    directive = directiveDe.injector.get(StickyDirective);
+
+    fixture.detectChanges();
+  });
+
+
+  it('should create a Sticky directive instance', () => {
     expect(directive).toBeTruthy();
   });
+
+  it('should add sticky css prop', () => {
+    const nativeElement: HTMLElement = directiveDe.nativeElement;
+    expect(nativeElement.style.position).toEqual('sticky');
+    expect(nativeElement.style.top).toEqual('0px');
+    expect(nativeElement.style.zIndex).toEqual('10');
+  });
+
+  describe('Elements setter', () => {
+
+    describe('setHTMLElement', () => {
+      it('should transform from string', () => {
+        directive.scrollContainer = 'body-container';
+
+        const scrollContainer = document.getElementById('body-container');
+        expect(directive.scrollContainer).toBe(scrollContainer as any);
+      });
+
+      it('should transform from ElementRef', () => {
+        directive.scrollContainer = component.scCont;
+
+        const scrollContainer: HTMLElement = component.scCont.nativeElement;
+        expect(directive.scrollContainer).toBe(scrollContainer as any);
+      });
+
+      it('should set the HTMLElement received', () => {
+        const scrollContainer = document.getElementById('body-container');
+        expect(directive.scrollContainer).toBe(scrollContainer as any);
+      });
+    });
+
+    describe('Setters', () => {
+      let setHTMLElementSpy: jasmine.Spy;
+
+      beforeEach(() => {
+        setHTMLElementSpy = spyOn(directive as any, 'setHTMLElement');
+      });
+
+      it('should the setter of scrollContainer call the setHTMLElement correctly', () => {
+        directive.scrollContainer = 'body-container';
+        expect(setHTMLElementSpy).toHaveBeenCalledWith('_scrollContainer', 'body-container');
+      });
+
+      it('should the setter of triggerOn call the setHTMLElement correctly', () => {
+        directive.triggerOn = 'trigger-here';
+        expect(setHTMLElementSpy).toHaveBeenCalledWith('_triggerOn', 'trigger-here');
+      });
+    });
+
+  });
+
 });
--- projects/sticky-directive/src/lib/sticky.directive.ts
+++ projects/sticky-directive/src/lib/sticky.directive.ts
@@ -1,10 +1,69 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit, ElementRef, Input } from '@angular/core';
 
+/**
+ * This directive puts a css class in a sticky element to improve usability.
+ * To understand how it works I highly recommend to read the article that was used to made this.
+ * Here you can understand better the architecture:
+ * https://developers.google.com/web/updates/2017/09/sticky-headers
+ */
 @Directive({
   selector: '[ngxSticky]'
 })
-export class StickyDirective {
+export class StickyDirective implements OnInit {
 
-  constructor() { }
+  /**
+   * * It must be a top container of the sticky element and of the element that will trigger the custom class on the sticky element.
+   * * This should be the element with the scroll
+   */
+  @Input()
+  get scrollContainer(): string | ElementRef | HTMLElement {
+    return this._scrollContainer;
+  }
+  set scrollContainer(value: string | ElementRef | HTMLElement) {
+    this.setHTMLElement('_scrollContainer', value);
+  }
+  private _scrollContainer: HTMLElement;
+
+  /**
+   * Element that will trigger the custom class on the sticky element.
+   */
+  @Input()
+  get triggerOn(): string | ElementRef | HTMLElement {
+    return this._triggerOn;
+  }
+  set triggerOn(value: string | ElementRef | HTMLElement) {
+    this.setHTMLElement('_triggerOn', value);
+  }
+  private _triggerOn: HTMLElement;
+
+  constructor(private stickyElement: ElementRef) { }
+
+  ngOnInit(): void {
+    this.makeSticky();
+  }
+
+  /**
+   * Transform if needed into HTMLElement a value given to be set in a property
+   */
+  private setHTMLElement(prop: string, value: string | ElementRef | HTMLElement): void {
+    if (typeof value === 'string') {
+      this[prop] = document.getElementById(value);
+    } else if (value instanceof ElementRef) {
+      this[prop] = value.nativeElement;
+    } else {
+      this[prop] = value;
+    }
+  }
+
+  /**
+   * Make the main element sticky.
+   * Put the necessary css to make it happen.
+   */
+  private makeSticky(): void {
+    const nativeElement: HTMLElement = this.stickyElement.nativeElement;
+    nativeElement.style.position = 'sticky';
+    nativeElement.style.top = '0'; // TODO: input
+    nativeElement.style.zIndex = '10'; // TODO: input
+  }
 
 }
